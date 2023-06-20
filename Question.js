98655589af314f904e13b3a135820d171212f049
RazanHammad97 <razanhammadq@gmail.com>
2023-06-20 12:42:03 +0300

    first commit

---
+++ Question.js
@@ -0,0 +1,35 @@
+let questions = [
+
+    {
+        title:"In a region of constant potential ?",
+        answers: ["there can be no charge inside the region.",
+        "the electric field is uniform.",
+        "the electric field is zero.",
+        "both (a) and (c) are correct."],
+        correctAnswer : "both (a) and (c) are correct.",
+    },
+    {
+        title:"In a region of constant potential ?",
+        answers: ["there can be no charge inside the region.",
+        "the electric field is uniform.",
+        "the electric field is zero.",
+        "both (a) and (c) are correct."],
+        correctAnswer : "both (a) and (c) are correct.",
+    },
+    {
+        title:"In a region of constant potential ?",
+        answers: ["there can be no charge inside the region.",
+        "the electric field is uniform.",
+        "the electric field is zero.",
+        "both (a) and (c) are correct."],
+        correctAnswer : "both (a) and (c) are correct.",
+    },
+    {
+        title:"In a region of constant potential ?",
+        answers: ["there can be no charge inside the region.",
+        "the electric field is uniform.",
+        "the electric field is zero.",
+        "both (a) and (c) are correct."],
+        correctAnswer : "both (a) and (c) are correct.",
+    },
+];
